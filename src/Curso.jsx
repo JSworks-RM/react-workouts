3923de926e1a8679857b1f5eb61cdd451377783f
Nacho RM <joseignaciorm13@gmail.com>
2020-02-03 20:01:25 +0100

    Arreglo componente presentacional...

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -9,7 +9,7 @@ const Curso = ( { title, image, price, profesor } )=> (
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
         <h3 className="t5 s-mb-2 s-center">
-            { profesor }
+            { `Prof. ${profesor}` }
         </h3>
         <div className="s-mb-2 s-main-center">
             <div className="card__teacher s-cross-center">
