3bb146d27b101f018cda050622dae13f31555a84
Nacho RM <joseignaciorm13@gmail.com>
2020-02-10 19:33:20 +0100

    Instalo librería axios, hacemos petición con axios

--- src/Components/Pages/Users.jsx
+++ src/Components/Pages/Users.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import UserCard from '../Molecules/UserCard'
+import axios from 'axios'
 
 class Users extends Component {
 
@@ -16,7 +17,7 @@ class Users extends Component {
     // Cuando el componente se monta, haya cargado, vamos a traer con un fetch (nativo de JavaScript) el código donde se necesita solo la url y un objeto con la configuración que en este caso necesita el método get
     // En el primer then() obtengo respuesta y ejecuto una promesa concatenada
     // Y esa promesa cuando se resuelve, nos va a traer el array de objeto que es el que guardaremos en el estado
-    componentDidMount () {
+    /* componentDidMount () {
         fetch ( 'https://jsonplaceholder.typicode.com/users', { method: 'get'} )
         .then( response =>  response.json() ) // Enviamos otra promesa a la respuesta. Para este ejemplo en la documentación lo requiere
         .then( response2 => {
@@ -25,6 +26,19 @@ class Users extends Component {
                 users: response2
             })
         }) // Aqui si nos devuelve la data
+    } */
+
+    // Misma peticion con axios en vez de fetch
+    componentDidMount () {
+        // Con axion indicamos directamente el método a usar y pasamos directamente la url
+        // axios devuelve un objeto como respuesta de la promesa directamente y nos trae la data sin necesidad de resolver dos promesas
+        // Entonces nos ahorramos un then, una promesa, de crear un objeto de configuración y actualizamos directamente el state
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+            this.setState({
+                users: res.data
+            })
+        })
     }
 
     render () {
