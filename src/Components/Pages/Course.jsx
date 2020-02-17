33a64db2266e7f645b0200a07da3e8f895c11b63
Nacho RM <joseignaciorm13@gmail.com>
2020-02-17 21:20:54 +0100

    Actualizando el estado con hooks de Course.jsx

--- src/Components/Pages/Course.jsx
+++ src/Components/Pages/Course.jsx
@@ -3,26 +3,31 @@ import React, { useState } from 'react'
 // Para obtener el parámetro que viene de la url se obtiene a través de la propiedad match que se encuentra dentro del objeto prop. Entonces destructoramos el objeto prop para leer sólo el atributo match
 const Course = ( { match } ) => { 
 
-    const [ state, setState ] = useState ({
-      currentCourse : 
-        {
-          "id": 1,
-          "titulo": "React desde cero",
-          "image": "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
-          "price": 40,
-          "profesor": "Beto Quiroga",
-          "avatar": "https://api.ed.team/files/avatars/38118146-4b4a-4844-8959-157614e04cd1.jpg"
-        }
+    const [ state, setState ] = useState ({ 
+        id: 1,
+        titulo: "React desde cero",
+        image: "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+        price: 40,
+        profesor: "Beto Quiroga",
+        avatar: "https://api.ed.team/files/avatars/38118146-4b4a-4844-8959-157614e04cd1.jpg"
     })
+
+    const changeTitle = text => {
+      setState ({
+        ...state,
+        titulo: text
+      })
+    }
 
     return (
         <div className="ed-grid m-grid-3">
         {
-            state.currentCourse ? (
+            state ? (
                 <>
-                    <h1 className="m-cols-3">{ state.currentCourse.titulo }</h1>
-                    <img className="m-cols-1" src={ state.currentCourse.image } alt={ state.currentCourse.titulo } />
-                    <p className="m-cols-2">Profesor: { state.currentCourse.profesor }</p>
+                    <h1 className="m-cols-3">{ state.titulo }</h1>
+                    <img className="m-cols-1" src={ state.image } alt={ state.titulo } />
+                    <p className="m-cols-2">Profesor: { state.profesor }</p>
+                    <button onClick={changeTitle.bind(this, 'Go Desde Cero')}>Cambiar título</button>
                 </>
             )
             :   <h1>El curso no existe</h1>
