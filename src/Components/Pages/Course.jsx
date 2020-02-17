83da0d46e07a3a9fef57c2eb4695faa5a693a002
Nacho RM <joseignaciorm13@gmail.com>
2020-02-17 21:48:43 +0100

    Utilizando el hook useState()

--- src/Components/Pages/Course.jsx
+++ src/Components/Pages/Course.jsx
@@ -1,23 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 // Para obtener el parámetro que viene de la url se obtiene a través de la propiedad match que se encuentra dentro del objeto prop. Entonces destructoramos el objeto prop para leer sólo el atributo match
 const Course = ( { match } ) => { 
 
-    const [ state, setState ] = useState ({ 
-        id: 1,
-        titulo: "React desde cero",
-        image: "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
-        price: 40,
-        profesor: "Beto Quiroga",
-        avatar: "https://api.ed.team/files/avatars/38118146-4b4a-4844-8959-157614e04cd1.jpg"
-    })
+    const [ state, setState ] = useState ({})
 
-    const changeTitle = text => {
-      setState ({
-        ...state,
-        titulo: text
-      })
-    }
+    // useEffect() va a ser llamado siempre que el componente se renderice creando un loop infinito. Por eso el segundo parámetro que recibe la función useEffect, como regla, le pasamos un array vacío [] para que se renderice sólo una sola vez; es una forma de simular el component didMount que corre solo una vez
+    // useEffect() recibe como primer parámetro un callback que en el bloque vamos a poder ejecutar la función que nosotros queramos. En nuestro ejemplo de course, vamos a hacer una petición http al servidor con axios y cuando la promesa se resuelva, nos devuelva una respuesta y con esa respuesta vamos a actualizar el estado.
+    useEffect (() => {
+      axios.get(`http://my-json-server.typicode.com/joseignaciorm/json-db/cursos/${match.params.id}`)
+        .then(res => setState ( res.data )) // Promesa donde ejecutamos setState para cambiar el estado actual de useState()
+        }, [] )
 
     return (
         <div className="ed-grid m-grid-3">
@@ -27,7 +21,6 @@ const Course = ( { match } ) => {
                     <h1 className="m-cols-3">{ state.titulo }</h1>
                     <img className="m-cols-1" src={ state.image } alt={ state.titulo } />
                     <p className="m-cols-2">Profesor: { state.profesor }</p>
-                    <button onClick={changeTitle.bind(this, 'Go Desde Cero')}>Cambiar título</button>
                 </>
             )
             :   <h1>El curso no existe</h1>
