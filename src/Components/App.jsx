834dbb0b4028e36b2572866ea3601457ec866b96
Nacho RM <joseignaciorm13@gmail.com>
2020-02-20 17:13:11 +0100

    Instalo Redux, React-redux. Creo Reducer component, método initialState. Export default método createStore y pasamos al reducer. Cambio nombre componente MainMenu por Header. Creo AppRoutes y refactorizo rutas de component App hacia AppRouter.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,55 +1,9 @@
 import React from 'react'
 import "../styles/styles.scss"
-
-// Components
-import Form from './Pages/Form'
-import Courses from './Pages/Courses'
-import Course from './Pages/Course'
-import History from './Pages/History'
-import Home from './Pages/Home'
-import Users from './Pages/Users'
-import Private from './Pages/Private'
-import Public from './Pages/Public'
-import Squarefiles from './Pages/SquareFiles'
-import UsersFetch from './Pages/UsersFetch'
-
-// Importando de la librería React Router los componentes "Router y Route"
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
-import MainMenu from './Organisms/Header'
-
+import AppRoutes from './AppRoutes'
 
 const App = () => (
-  // Envolvemos nuestra app dentro del componente Router
-  // Dentro del componente Router declaramos rutas y cada una de las rutas también va a ser un llamado a un componente
-  // El componente ruta "Route" necesita dos propiedades "Props": 
-  // 1: El path="/" (La ruta en el navegador del componente que queremos leer)
-  // 2: Atributo booleano, por defecto es false para declarar que la ruta es exacta se pone "exact"
-  // Por último indicamos que componente vamos a imprimir en esa ruta. ej: component={Banner}
-  // Con el componente Switch evitamos que se pinte una ruta default en todas las demás rutas.
-  // Para pasar un parámetro a una ruta se usa la sintaxis :parametro
-  <Router>
-    <MainMenu />
-    <Switch>
-      <Route path="/" exact component={ Home } />
-      <Route path="/cursos/:id" component={ Course } />
-      <Route path="/cursos" component={ Courses } />
-      <Route path="/historial/:valor" component={ History } />
-      <Route path="/historial" component={ History } />      
-      <Route path="/usuarios" component={ Users } />
-      <Route path="/private" component={ Private } />
-      <Route path="/public" component={ Public } />
-      <Route path="/users" component={ UsersFetch } />
-      <Route path="/squarefiles" component={ Squarefiles } />
-      <Route path="/formulario" component={ () => <Form name={'Página de contacto'} /> } />
-      <Route component={() => (
-        <div className="ed-grid">
-          <h3>Error 404</h3>
-          <span>Página no encontrada</span>
-        </div>
-      )} />
-    </Switch> 
-  </Router>
-
+  <AppRoutes />
 )
 
 export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { createStore } from 'redux' // Importamos método createStore de librería redux
+
+// Creamos el reducer que es el que va a hacer los cambios en el store
+// Reducer para poder manejar el estado recibo el estado inicial y recibe la acción que debe ejecutar para modificar el estado
+// Con esa información el va a retornar un nuevo estado
+// Como la primera vez que cargue el estado no va a existir, para que no devuelva undefined le indicaremos que su estado va a ser igual a un estado inicial
+// Para saber que es inicialStore debemos declararlo, sino igual sería un undefined
+const initialStore = {
+    cart: []
+}
+
+const rootReducer = ( state = initialStore, action ) => {
+    return state
+}
+
+// Exportamos la función createStore() que importamos de redux y le pasamos al reducer
+export default createStore(rootReducer)
