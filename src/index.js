654afe452876ec9ffabfc90dbc9c9b576afa16c7
joseignaciorm <47498082+joseignaciorm@users.noreply.github.com>
2024-04-21 19:48:50 +0000

    Add vscode extension prettier. setup settings.json file.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App'; 
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./Components/App"
 
-const root = document.getElementById('root')
-    // El método createElemet de react nos permite crear un elemento y renderizarlo en pantalla. Este método recibe tres parámetros (el componente a crear, las propiedades que va a tener este componente y recibe todos los hijos que va a tener este componente)
-    //elemento = React.createElement(componente, propiedades, hijos)
-    //elemento = React.createElement("h1", { className: "saludo" }, "Hola Mundo")
+const root = document.getElementById("root")
+// El método createElemet de react nos permite crear un elemento y renderizarlo en pantalla. Este método recibe tres parámetros (el componente a crear, las propiedades que va a tener este componente y recibe todos los hijos que va a tener este componente)
+//elemento = React.createElement(componente, propiedades, hijos)
+//elemento = React.createElement("h1", { className: "saludo" }, "Hola Mundo")
 
 // Método render renderiza algún elemento html y necesita como parámetros un elemento para renderizar y un lugar en el DOM dónde queremos insertar dicho elemento
-ReactDOM.render( <App />, root );
+ReactDOM.render(<App />, root)
